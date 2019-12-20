432c56089ba9584d95cda542facaa1a8f0a87b6e
new <new@192.168.100.11>
2019-12-20 15:50:03 +0530

    modified

--- Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
+++ Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sathya.rms.services.EmployeeService;
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 
 class RmsApplicationTests {
