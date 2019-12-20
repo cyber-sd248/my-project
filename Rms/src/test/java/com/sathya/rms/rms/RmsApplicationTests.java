02c3e47e8a5e8e2e7f1d62bad6e856b0d270b944
new <new@192.168.100.11>
2019-12-20 15:36:25 +0530

    testcase modified

--- Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
+++ Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
@@ -1,8 +1,8 @@
 package com.sathya.rms.rms;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.sathya.rms.services.EmployeeService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+
 class RmsApplicationTests {
 	
 	@Autowired
