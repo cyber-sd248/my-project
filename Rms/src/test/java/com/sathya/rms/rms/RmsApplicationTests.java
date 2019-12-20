8db4b8ad6d8f6f4d41956b9774647e5b1c26c018
new <new@192.168.100.11>
2019-12-20 15:13:45 +0530

    Rms testcase file import files are modified

--- Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
+++ Rms/src/test/java/com/sathya/rms/rms/RmsApplicationTests.java
@@ -1,15 +1,15 @@
 package com.sathya.rms.rms;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.sathya.rms.services.EmployeeService;
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class RmsApplicationTests {
 	
